5ce173c7aff4ccd552bc6cf42a4e044dac95a258
RJ Garcia <ragboyjr@icloud.com>
2026-01-04 14:04:02 -0500

    chore: bumping min versions / setup testing matrix
    - bumped up min versions we want to support
    - started testing on later kotlin and kotlinx.serialization versions for
    compatability

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,18 @@ rootProject.name = "Codanbaru - DynaMap"
 
 include("dynamap-core")
 include("dynamap-tests")
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        // we're intentionally just overriding some versions at runtime for ci testing matrix
+        create("libs") {
+            providers.gradleProperty("kotlin.version.override").orNull?.let {
+                version("kotlin", it)
+            }
+
+            providers.gradleProperty("kotlinx.serialization.version.override").orNull?.let {
+                version("kotlinx-serialization", it)
+            }
+        }
+    }
+}
